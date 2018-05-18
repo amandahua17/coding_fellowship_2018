56ff9a2b50bd57e3fd2863960df8857255f663f1
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-05-18 14:59:59 -0500

    Calculator PHP Project Finished!
    Added a drop down menu with operators!

--- calculator.php
+++ calculator.php
@@ -8,12 +8,34 @@
 	echo "
 		<form action=''>
 			<input type = 'text' name='variable1' />
+			<select name = 'operation'>
+				<option value='+'>+</option>
+				<option value='-'>-</option>
+				<option value='*'>*</option>
+				<option value='/'>/</option>
+				<option value='%'>%</option>
+			</select>
 			<input type = 'text' name='variable2' />
 			<input type = 'submit' />
 		</form>
 	";
 
 	if(isset($_REQUEST['variable1'])){
-		echo "answer: ".add($_REQUEST['variable1'], $_REQUEST['variable2']);
+		if ($_REQUEST['operation'] == '+'){
+			$answer = $_REQUEST['variable1'] + $_REQUEST['variable2'];
+		}
+		if ($_REQUEST['operation'] == '-'){
+			$answer = $_REQUEST['variable1'] - $_REQUEST['variable2'];
+		}
+		if ($_REQUEST['operation'] == '*'){
+			$answer = $_REQUEST['variable1'] * $_REQUEST['variable2'];
+		}
+		if ($_REQUEST['operation'] == '/'){
+			$answer = $_REQUEST['variable1'] / $_REQUEST['variable2'];
+		}
+		if ($_REQUEST['operation'] == '%'){
+			$answer = $_REQUEST['variable1'] % $_REQUEST['variable2'];
+		}
+		echo"$_REQUEST[variable1] $_REQUEST[operation] $_REQUEST[variable2] = $answer";
 	}
 ?>
