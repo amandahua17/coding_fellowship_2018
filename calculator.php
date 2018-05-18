609414bcc921d07c730f21726b34b42b4afef922
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-05-18 14:20:05 -0500

    done with meeting

--- calculator.php
+++ calculator.php
@@ -1,13 +1,19 @@
 <?php
 
-	$Test = "hello";
+	function add($variable1, $variable2){
+		return($variable1+$variable2);
+	}
 
+
+	echo "
+		<form action=''>
+			<input type = 'text' name='variable1' />
+			<input type = 'text' name='variable2' />
+			<input type = 'submit' />
+		</form>
+	";
+
+	if(isset($_REQUEST['variable1'])){
+		echo "answer: ".add($_REQUEST['variable1'], $_REQUEST['variable2']);
+	}
 ?>
-
-<h1> title </h1>
-
-<?php
-
-echo $Test;
-
- ?>
