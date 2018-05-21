c43590f7cb1cf53e71975185fd394311a959807b
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-05-21 10:40:34 -0500

    Added Helpines
    See if I can fix some formatting issues, added information to the page.  Progressing on to sounds or exercises page next, probably.

---
+++ exercises.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>exercises</title>
+		<link rel='stylesheet' href='style.css'/>
+	</head>
+	<body class='bg'>
+		<h1>exercises</h1>
+		<p class="infoSection">hi! sorry, this page is currently under construction, but please feel free to visit one of the other pages in the mean time.</p>
+
+		<a class='home'href="index.html">back to home</a>
+	</body>
+</html>
