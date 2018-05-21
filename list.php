2d004611bde299aeebf1a1ccc4f533a5a08f4540
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-05-21 08:47:39 -0500

    Edits to List
    Changed formatting of list (ordered list instead of paragraph, less tedious)

--- list.php
+++ list.php
@@ -15,13 +15,24 @@
 		<div class="infoSection">
 			<p>the author of this page has written down a personal list of steps for feeling better. we note that not everyone works the same way, and this is only a list of suggestions. feel free to add to the list below.</p>
 			<h2>suggestions:</h2>
-			<p>1. give yourself a short period of time to wallow in negative feeling <br> (suggested times: 5 minutes, 15 minutes)<br><br>
+			<!--<p>1. give yourself a short period of time to wallow in negative feeling <br> (suggested times: 5 minutes, 15 minutes)<br><br>
 			2. take a shower<br><br>
 			3. play some upbeat music<br>(suggested playlists: <a target='_blank'href='https://open.spotify.com/user/22k343m6upq3jz5sylnsoshsa/playlist/55vFMBQnGhzfabmeExGle3?si=dAwY38qRQq6cJMbVQI07CQ'>Spotify playlist</a>)<br><br>
 			4. get dressed immediately, turn on the lights, eat something<br><br>
 			5. open the windows/shades<br><br>
 			6. drink some cold water<br><br>
-			7. if it's sunny, go outside<br><br>
+			7. if it's sunny, go outside<br><br>-->
+			<ol>
+				<li>give yourself a short period of time to wallow in negative feeling<br>
+					(suggested times: 5 minutes, 15 minutes)</li><br>
+				<li>take a shower</li><br>
+				<li>play some upbeat music<br>
+					(suggested playlists: <a target='_blank'href='https://open.spotify.com/user/22k343m6upq3jz5sylnsoshsa/playlist/55vFMBQnGhzfabmeExGle3?si=dAwY38qRQq6cJMbVQI07CQ'>Spotify playlist</a>)</li><br>
+				<li>get dressed immediately, turn on the lights, eat something</li><br>
+				<li>open the windows/shades</li><br>
+				<li>drink some cold water</li><br>
+				<li>if it's sunny, go outside</li><br>
+			</ol>
 			<?php
 			/*	while(true){
 					$placeholder = $_REQUEST['suggestion'];
