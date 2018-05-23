44a6aee6e0e23faab76008cd489c7514f0631a36
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-05-23 09:50:03 -0500

    Needed to show an example using a demo branch

---
+++ exercises.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>exercises</title>
+		<link rel='stylesheet' href='style.css'/>
+	</head>
+	<body class='bg'>
+		<h1>exercises</h1>
+		<div>
+			<h3>if you're feeling sleepy or low-energy</h3>
+			<a href='https://www.youtube.com/watch?v=r7oBejx1PHM'>tuck jumps x 5</a>
+			<h3>for frustration or disappointment</h3>
+			<p>go for a run</p>
+
+		</div>
+		<a class='home'href="index.html">back to home</a>
+	</body>
+</html>
---
+++ navbarexample.php
@@ -0,0 +1,20 @@
+<?php
+	function navbar(){
+		echo"
+			<h1>
+		";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
