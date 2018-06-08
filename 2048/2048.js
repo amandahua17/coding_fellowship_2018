6d85cb226ba8bc989cbfc4fb273f85b055722614
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-06-08 16:07:43 -0500

    box changes color based on position

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,38 +1,4 @@
 
-
-var body = document.querySelector('body');
-var key;
-// var b1 = document.getElementById("box");
-// updateBox(b1, box);
-	// console.log('marker');
-body.addEventListener("keydown", move);
-function move(event) {
-	var b1 = document.getElementById("box");
-	// console.log('hi');
-    key = event.keyCode || event.which;
-	console.log(key);
-
-	// var box1 = new box();
-	if(key == 37){
-		box.moveLeft();
-	}
-	if(key==38){
-		box.moveUp();
-	}
-	if(key==39){
-		box.moveRight();
-	}
-	if(key==40){
-		box.moveDown();
-	}
-	updateBox(b1, box);
-}
-
-function updateBox(b, boxn){
-	b.style.top = boxn.ypos + 'px';
-	b.style.left = boxn.xpos + 'px';
-}
-
 var box = {
 	xpos: Math.floor(Math.random()*8)*100,
 	ypos: Math.floor(Math.random()*8)*100,
@@ -57,22 +23,115 @@ var box = {
 		}
 	}
 };
+var body = document.querySelector('body');
+var key;
+var b1 = document.getElementById("box");
+	updateBox(b1);
+// var
 
-// function moveleft(element){
-// 	console.log("left");
-// 	if(element.style.left != 0){
-//
-// 	}
-// }
-//
-// function moveup(element){
-// 	console.log("up");
-// }
-//
-// function moveright(element){
-// 	console.log("right");
-// }
-//
-// function movedown(element){
-// 	console.log("down");
-// }
+body.addEventListener("keydown", move);
+function move(event) {
+	// console.log('hi');
+    key = event.keyCode || event.which;
+	// console.log(key);
+	// var box1 = new box();
+	if(key == 37){
+		box.moveLeft();
+	}
+	if(key==38){
+		box.moveUp();
+	}
+	if(key==39){
+		box.moveRight();
+	}
+	if(key==40){
+		box.moveDown();
+	}
+	updateBox(b1);
+}
+
+function updateBox(b){
+	b.style.top = box.ypos + 'px';
+	b.style.left = box.xpos + 'px';
+	b.style.backgroundColor = '#'+ getBoxColor(box.xpos, box.ypos).toString(16);
+
+	// checkDots(box.ypos, box.xpos);
+}
+
+function getBoxColor(xpos, ypos){
+	// console.log(xpos, ypos);
+	var xcol;
+	var ycol;
+	switch (xpos){
+		case 0:
+			xcol = 0x171;
+			break;
+		case 100:
+			xcol = 0x371;
+			break;
+		case 200:
+			xcol = 0x571;
+			break;
+		case 300:
+			xcol = 0x771;
+			break;
+		case 400:
+			xcol = 0x971;
+			break;
+		case 500:
+			xcol = 0xa71;
+			break;
+		case 600:
+			xcol = 0xc71;
+			break;
+		case 700:
+			xcol = 0xe71;
+			break;
+		default:
+			xcol = 0x070;
+	}
+
+	switch (ypos){
+		case 0:
+			ycol = 0x171;
+			break;
+		case 100:
+			ycol = 0x173;
+			break;
+		case 200:
+			ycol = 0x175;
+			break;
+		case 300:
+			ycol = 0x177;
+			break;
+		case 400:
+			ycol = 0x179;
+			break;
+		case 500:
+			ycol = 0x17a;
+			break;
+		case 600:
+			ycol = 0x17c;
+			break;
+		case 700:
+			ycol = 0x17e;
+			break;
+		default:
+			ycol = 0x070;
+	}
+	var col = ycol+xcol;
+	// console.log(col.toString(16));
+	return col;
+}
+
+function checkDots(){
+
+}
+
+function newDot(){
+	var dot = document.createElement("div");
+	var parent = document.getElementById("container");
+	dot.style.width = "10px";
+	dot.class = 'dot';
+	parent.appendChild(dot);
+}
