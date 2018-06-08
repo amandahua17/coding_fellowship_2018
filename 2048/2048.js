9120ee557f5f3561ca12087810e65e44ff9d3525
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-06-08 14:45:29 -0500

    got the box responding to keystrokes

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,37 +1,78 @@
+
+
 var body = document.querySelector('body');
 var key;
+// var b1 = document.getElementById("box");
+// updateBox(b1, box);
 	// console.log('marker');
-body.addEventListener("keypress", move());
+body.addEventListener("keydown", move);
 function move(event) {
+	var b1 = document.getElementById("box");
 	// console.log('hi');
     key = event.keyCode || event.which;
 	console.log(key);
+
+	// var box1 = new box();
 	if(key == 37){
-		moveleft();
+		box.moveLeft();
 	}
 	if(key==38){
-		moveup();
+		box.moveUp();
 	}
 	if(key==39){
-		moveright();
+		box.moveRight();
 	}
 	if(key==40){
-		movedown();
+		box.moveDown();
 	}
+	updateBox(b1, box);
 }
 
-function moveleft(){
-	console.log("left");
+function updateBox(b, boxn){
+	b.style.top = boxn.ypos + 'px';
+	b.style.left = boxn.xpos + 'px';
 }
 
-function moveup(){
-	console.log("up");
-}
+var box = {
+	xpos: Math.floor(Math.random()*8)*100,
+	ypos: Math.floor(Math.random()*8)*100,
+	moveLeft: function(){
+		if(this.xpos!=0){
+			this.xpos-=100;
+		}
+	},
+	moveRight: function(){
+		if(this.xpos!=700){
+			this.xpos+=100;
+		}
+	},
+	moveUp: function(){
+		if(this.ypos!=0){
+			this.ypos-=100;
+		}
+	},
+	moveDown: function(){
+		if(this.ypos!=700){
+			this.ypos+=100;
+		}
+	}
+};
 
-function moveright(){
-	console.log("right");
-}
-
-function movedown(){
-	console.log("down");
-}
+// function moveleft(element){
+// 	console.log("left");
+// 	if(element.style.left != 0){
+//
+// 	}
+// }
+//
+// function moveup(element){
+// 	console.log("up");
+// }
+//
+// function moveright(element){
+// 	console.log("right");
+// }
+//
+// function movedown(element){
+// 	console.log("down");
+// }
