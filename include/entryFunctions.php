6dca4becec3fe483edf5c30c1eae39f9fd991fa1
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-06-20 08:58:06 -0500

    More style adjustments

--- include/entryFunctions.php
+++ include/entryFunctions.php
@@ -22,35 +22,56 @@
 	//VALIDATION FUNCTIONS
 	function validateSong($i){
 		$errors = array();
+		$errors+=validateTextField('songTitle');
+		$errors+=validateTextField('songArtist');
+		// $errors+=validateTextField('songLink');		//OPTIONAL
 		return $errors;
 	}
 
 	function validatePhoto($i){
 		$errors = array();
+		$errors+=validateTextField('photoPath');
+		$errors+=validateTextField('photographer');
+		// $errors+=validateTextField('photoTitle');	//OPTIONAL
 		return $errors;
 	}
 
 	function validatePerson($i){
 		$errors = array();
+		$errors+=validateTextField('personName');
+		// $errors+=validateTextField('personRelationship');	//OPTIONAL
+		$errors+=validateTextField('personDescription');
 		return $errors;
 	}
 
 	function validateBook($i){
 		$errors = array();
+		$errors+=validateTextField('bookTitle');
+		$errors+=validateTextField('bookAuthor');
+		$errors+=validateTextField('bookNotes');
 		return $errors;
 	}
 
 	function validateMeal($i){
 		$errors = array();
+		$errors+=validateTextField('mealPlace');
+		$errors+=validateTextField('mealChef');
+		$errors+=validateTextField('mealDescription');
 		return $errors;
 	}
 
 	function validateOccasion($i){
 		$errors = array();
+		$errors+=validateTextField('occasionName');
+		$errors+=validateTextField('occasionDescription');
 		return $errors;
 	}
 
 	function validateProject($i){
 		$errors = array();
+		$errors+=validateTextField('projectTitle');
+		$errors+=validateTextField('projectProgress');
+		$errors+=validateTextField('projectPartners');
+		$errors+=validateTextField('projectNotes');
 		return $errors;
 	}
