8603f5f4352b381f7c43c7281f0fc7637d7a5cf4
amandahualacrm <39344015+amandahualacrm@users.noreply.github.com>
2018-06-21 09:44:48 -0500

    file path changes: don't use the first / because the root is different!

--- include/include_all.php
+++ include/include_all.php
@@ -2,19 +2,19 @@
 
 	date_default_timezone_set('America/Chicago');
 
-	include('/config/config.php');
-	include('/include/db_query.php');
+	include('config/config.php');
+	include('include/db_query.php');
 
-	include('/include/htmlFunctions.php');
-	include('/include/userFunctions.php');
-	include('/include/entryFunctions.php');
-	include('/include/songFunctions.php');
-	include('/include/photoFunctions.php');
-	include('/include/bookFunctions.php');
-	include('/include/occasionFunctions.php');
-	include('/include/formFunctions.php');
-	include('/include/timelineFunctions.php');
-	include('/include/dateFunctions.php');
+	include('include/htmlFunctions.php');
+	include('include/userFunctions.php');
+	include('include/entryFunctions.php');
+	include('include/songFunctions.php');
+	include('include/photoFunctions.php');
+	include('include/bookFunctions.php');
+	include('include/occasionFunctions.php');
+	include('include/formFunctions.php');
+	include('include/timelineFunctions.php');
+	include('include/dateFunctions.php');
 
 	session_start();
 	// file_uploads = On;
